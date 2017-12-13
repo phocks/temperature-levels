544ca736872fa3853ff1564ac349b6560b5034d7
Joshua Byrd <phocks@gmail.com>
2017-12-13 15:56:46 +1000

    Spanify module added

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 const { h, Component } = require("preact");
+const spanify = require("spanify");
+
+
 const styles = require("./App.scss");
 
 const Temperature = require("./Temperature");
@@ -25,6 +28,8 @@ class App extends Component {
 
     this.checkLocalStorage();
 
+    
+
     // We need to bind this to class functions sometimes
     this.handleAgeChange = this.handleAgeChange.bind(this);
   }
@@ -34,63 +39,7 @@ class App extends Component {
     this.saveLocalSession(year);
     this.setState(prevState => ({ birthYear: year }));
 
-    const myElement = document.querySelector('[name="classify"]');
-    const paragraph = myElement.nextElementSibling;
-
-    const anchors = paragraph.querySelectorAll("a");
-    console.log(anchors);
-
-    anchors.forEach(anchor => {
-      if (anchor.innerHTML !== " ") return;
-
-      const elementTitle = anchor.getAttribute("title");
-
-      if (elementTitle.slice(0, 3) === "end") {
-        anchor.parentNode.removeChild(anchor);
-        return;
-      }
-
-      const spanEl = document.createElement("span");
-      spanEl.setAttribute("class", elementTitle);
-      const spanTextEl = anchor.nextSibling;
-      spanEl.innerHTML = spanTextEl.textContent.trim()
-      console.log(spanEl);
-
-      anchor.parentNode.appendChild(spanEl);
-
-      anchor.parentNode.replaceChild(spanEl, anchor);
-      spanTextEl.parentNode.removeChild(spanTextEl);
-    });
-
-    // console.log(paragraph.innerHTML);
-    // paragraph.innerHTML = paragraph.innerHTML.replace('<a href="#" target="_self" title="', '<span class="');
-    // paragraph.innerHTML = paragraph.innerHTML.replace('<a href="#" target="_self" title="endfield"> </a>', '</span>');
-    // console.log(paragraph.innerHTML);
-
-    // const span = document.createElement("span");
-    // span.innerHTML = year;
-    // const strong = myElement.nextElementSibling.querySelector("strong");
-    // const dir = myElement.nextElementSibling.querySelector('[dir="ltr"]');
-
-    // console.log(strong, myElement.nextElementSibling.innerHTML);
-
-    // // strong.parentNode.replaceChild(span, strong);
-    // strong.innerHTML = year;
-
-    // dir.innerHTML = "not many";
-
-    // if (
-    //   myElement.nextElementSibling.innerHTML ===
-    //   "Now kids experience really hot temperatures."
-    // ) {
-    //   setTimeout(() => {
-    //     myElement.nextElementSibling.innerHTML = `When you were young the temperature was not that hot.`;
-    //   }, 1000);
-    // } else {
-    //   setTimeout(() => {
-    //     myElement.nextElementSibling.innerHTML = `Now kids experience really hot temperatures.`;
-    //   }, 1000);
-    // }
+    spanify.spanify();
   }
 
   saveLocalSession(year) {
@@ -164,5 +113,39 @@ function localStorageTest() {
     return false;
   }
 }
+
+// function spanify() {
+//   /*
+//    * Possibly limit it to certain paragraphs
+//    * Make it scan the hole page for now
+//    */
+//   // const myElement = document.querySelector('[name="classify"]');
+//   // const paragraph = myElement.nextElementSibling;
+
+//   const anchors = document.querySelectorAll("a");
+
+//   anchors.forEach(anchor => {
+//     if (anchor.innerHTML !== " ") return;
+
+//     const elementTitle = anchor.getAttribute("title");
+
+//     if (!elementTitle) return;
+
+//     if (elementTitle.slice(0, 3) === "end") {
+//       anchor.parentNode.removeChild(anchor);
+//       return;
+//     }
+
+//     const spanEl = document.createElement("span");
+//     spanEl.setAttribute("class", elementTitle);
+//     const spanTextEl = anchor.nextSibling;
+//     spanEl.innerHTML = spanTextEl.textContent.trim();
+
+//     anchor.parentNode.appendChild(spanEl);
+
+//     anchor.parentNode.replaceChild(spanEl, anchor);
+//     spanTextEl.parentNode.removeChild(spanTextEl);
+//   });
+// }
 
 module.exports = App;
