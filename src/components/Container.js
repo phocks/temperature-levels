80416d6e2a090df10d6419fbab914f1b6537d3b4
Joshua Byrd <phocks@gmail.com>
2017-12-15 11:10:49 +1000

    Remove the remove

--- src/components/Container.js
+++ src/components/Container.js
@@ -4,15 +4,15 @@ const styles = require("./Container.scss");
 
 class Container extends Component {
   componentDidMount() {
-    var el = this.base.parentNode;
+    // var el = this.base.parentNode;
 
-    // get the element's parent node
-    var parent = el.parentNode;
+    // // get the element's parent node
+    // var parent = el.parentNode;
 
-    // move all children out of the element
-    while (el.firstChild) parent.insertBefore(el.firstChild, el);
+    // // move all children out of the element
+    // while (el.firstChild) parent.insertBefore(el.firstChild, el);
 
-    // remove the empty element
+    // // remove the empty element
     // parent.removeChild(el);
   }
 
