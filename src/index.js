08d553ae1b555f4c05e34ac1c1dc69d79e638c33
Joshua Byrd <phocks@gmail.com>
2018-08-10 15:48:14 +1000

    Get data from other article

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const { h, render } = require("preact");
-const axios = require("axios"); // Maybe use xhr instead
 const xhr = require("xhr");
 
 const PROJECT_NAME = "interactive-temperature-records";
@@ -7,30 +6,8 @@ const root = document.querySelector(`[data-${PROJECT_NAME}-root]`);
 
 let content = {}; // Using xhr to get a CoreMedia page
 
-// Fetch html from external html and inject in page
-axios
-  .get("https://raw.githubusercontent.com/TLes/temprecord/master/script.html")
-  .then(response => {
-    const injectHtml = response.data;
-    const injectionRoot = document.querySelector(`[name="fullscript"]`);
-
-    injectionRoot.innerHTML = injectHtml;
-    // + `<div class="birthyearselect u-full"></div>`;
-
-    // get the element's parent node
-    var parent = injectionRoot.parentNode;
-
-    // move all children out of the element
-    while (injectionRoot.firstChild)
-      parent.insertBefore(injectionRoot.firstChild, injectionRoot);
-
-    // remove the empty element
-    parent.removeChild(injectionRoot);
-
-    init();
-  });
-
 function init() {
+  console.log(window.__ODYSSEY__.utils.misc.smartquotes);
   const App = require("./components/App");
   render(
     <App content={content} projectName={PROJECT_NAME} />,
@@ -41,45 +18,49 @@ function init() {
 
 // Fetch another CoreMedia article and parse it for dynamic use
 xhr({ url: root.getAttribute("data-content-url") }, (err, response, body) => {
-  const doc = new DOMParser().parseFromString(body, "text/html");
-  const startNode = doc.querySelector('a[name="content"]');
-  const endNode = doc.querySelector('a[name="endcontent"]');
+  function transform() {
+    const doc = new DOMParser().parseFromString(body, "text/html");
+    const startNode = doc.querySelector('a[name="content"]');
+    const endNode = doc.querySelector('a[name="endcontent"]');
 
-  if (!startNode || !endNode) {
-    console.error(new Error("No content bookends found in document."));
+    if (!startNode || !endNode) {
+      console.error(new Error("No content bookends found in document."));
 
-    return init();
-  }
-
-  let currentNode = startNode;
-  let currentSection;
-
-  while (
-    ((currentNode = currentNode.nextSibling),
-    currentNode && currentNode !== endNode)
-  ) {
-    if (
-      !currentNode.tagName ||
-      (currentNode.tagName === "P" &&
-        currentNode.textContent.trim().length === 0)
-    ) {
-      // Skip non-elements & empty paragraphs
-    } else if (
-      currentNode.tagName === "A" &&
-      currentNode.getAttribute("name").length > 0
-    ) {
-      // Set currentSection to tag's name attribute, and create a key on content if it doesn't exist
-      currentSection = currentNode.getAttribute("name");
-      content[currentSection] = content[currentSection] || [];
-    } else if (currentSection) {
-      // Append element to content's currentSection
-      content[currentSection].push(currentNode);
+      return init();
     }
+
+    let currentNode = startNode;
+    const injectionRoot = document.querySelector('[name="fullscript"]');
+
+    let fetchedNodes = [];
+
+    while (
+      ((currentNode = currentNode.nextSibling),
+      currentNode && currentNode !== endNode)
+    ) {
+      fetchedNodes.push(currentNode);
+    }
+
+    fetchedNodes.forEach(node => {
+      // Use Odyssey API to re-apply smart quotes
+      window.__ODYSSEY__.utils.misc.smartquotes(node);
+
+      // Append fetched content
+      injectionRoot.appendChild(node);
+    });
+
+    // Unwraps injected content from parent
+    var parent = injectionRoot.parentNode;
+    while (injectionRoot.firstChild)
+    parent.insertBefore(injectionRoot.firstChild, injectionRoot);
+    parent.removeChild(injectionRoot);
   }
 
-  console.log(content);
-
-  // init();
+  if (window.__ODYSSEY__) {
+    transform();
+  } else {
+    window.addEventListener("odyssey:api", transform);
+  }
 });
 
 if (module.hot) {
